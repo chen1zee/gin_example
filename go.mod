af9bdcde4fcf33273df63cd58c5efebdb8105d31
chen1zee <chen2zee1@gmail.com>
2019-08-06 17:59:59 +0800

    [init]

---
+++ go.mod
@@ -0,0 +1,11 @@
+module gin_example
+
+go 1.12
+
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/go-ini/ini v1.46.0
+	github.com/jinzhu/gorm v1.9.10 // indirect
+	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337 // indirect
+	gopkg.in/ini.v1 v1.46.0 // indirect
+)
---
+++ src/gin-blog/pkg/util/pagination.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"gin_example/src/gin-blog/pkg/setting"
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+func GetPage(c *gin.Context) int {
+	result := 0
+	pageInt64, _ := strconv.ParseInt(c.Query("page"), 10, 0)
+	page := int(pageInt64)
+	if page > 0 {
+		result = (page - 1) * setting.PageSize
+	}
+	return result
+}
